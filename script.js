b907b7741d04904d2d56ab402ee7276604dbc08d
MD. Tamjid Bond <89590805+tamjidbond@users.noreply.github.com>
2024-03-07 04:05:50 +0600

    anti-inspect

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.onkeydown=function(e){
+    if(e.keyCode == 123){
+        return false
+    } 
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+        return false;
+    }
+}
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
